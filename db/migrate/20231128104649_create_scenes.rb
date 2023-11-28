29531cb71c07355ec3e85362700c93099825cc08
barbara oliveira <barbarafictiovore@gmail.com>
2023-11-28 17:25:46 +0100

    index reviews all

--- db/migrate/20231128104649_create_scenes.rb
+++ db/migrate/20231128104649_create_scenes.rb
@@ -3,11 +3,11 @@ class CreateScenes < ActiveRecord::Migration[7.1]
     create_table :scenes do |t|
       t.references :place, null: false, foreign_key: true
       t.references :movie, null: false, foreign_key: true
-      t.references :user, null: false, foreign_key: true
-      t.float :rating
+      t.references :user, null: true, foreign_key: true
+      t.float :rating, default: 0
       t.string :content
       t.float :price
-      t.boolean :payant
+      t.boolean :payant, default: false
       t.string :time
       t.string :link
 

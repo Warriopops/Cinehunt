94f7c5fca02a7b21e0cccbf0f4d245171b3874d6
lilian chabaus <lilian.chbd@gmail.com>
2023-11-28 13:38:53 +0100

    contr

--- app/models/scene.rb
+++ app/models/scene.rb
@@ -2,4 +2,7 @@ class Scene < ApplicationRecord
   belongs_to :place
   belongs_to :movie
   belongs_to :user
+  validates :rating, presence: true
+  validates :content, presence: true
+  validates :payant, presence: true
 end

93055d22f507219f13bbe36ff38859ba144dc62d
Hilham Behillil <hilhambehillilcw@gmail.com>
2023-11-28 15:45:38 +0100

    Seed bientot fini

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_28_104952) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_28_141740) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -70,6 +70,7 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_28_104952) do
     t.string "link"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "title"
     t.index ["movie_id"], name: "index_scenes_on_movie_id"
     t.index ["place_id"], name: "index_scenes_on_place_id"
     t.index ["user_id"], name: "index_scenes_on_user_id"
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,50 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+
+puts "start"
+Scene.destroy_all
+Place.destroy_all
+Movie.destroy_all
+User.destroy_all
+
+puts "create first user"
+user = User.create(email: "contact@cinehunt", password: "123456")
+puts "user created"
+
+joker = Movie.create(title: 'Joker', category: 'Drame')
+la_maison_du_mal = Movie.create(title: 'La Maison du mal', category: 'Horreur')
+barbie = Movie.create(title: 'Barbie', category: 'Comedie')
+
+new_jersey = Place.create(country: 'USA', city: 'Newark, New Jersey')
+los_angeles = Place.create(country: 'USA', city: 'Los Angeles')
+hertfordshire = Place.create(country: 'Royaume-Uni', city: 'Hertfordshire')
+bronx = Place.create(country: 'USA', city: 'Bronx')
+
+Scene.create(
+  movie: joker,
+  place: new_jersey,
+  user: user,
+  title: "Loew's Jersey Theatre",
+  content: "C’est ici qu’Arthur Fleck descend le tapis rouge en faisant des cabrioles",
+  rating: 4.4,
+  price: "9€",
+)
+
+Scene.create(
+  movie: joker,
+  place: bronx,
+  user: user,
+  title: "Les escaliers du Joker",
+  content: "Le célèbre escalier qui illustre l'affiche du film",
+  price: "Gratuit"
+)
+
+Scene.create(
+  movie: joker,
+  place: new_jersey,
+  title: "The Paramount Theater"
+  content: "Abandonné depuis, "
+
+)

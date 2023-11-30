feff75a9ede728eee8d9a9afa05ddbc142b1983d
barbara oliveira <barbarafictiovore@gmail.com>
2023-11-30 15:16:26 +0100

    add photo to seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,8 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+require "open-uri"
 ActiveRecord::Base.connection.disable_referential_integrity do
 
 puts "start"
@@ -25,7 +27,7 @@ barbie = Movie.create!(title: 'Barbie', category: 'Comedie')
 
 new_jersey = Place.create!(country: 'USA', city: 'Newark, New Jersey')
 los_angeles = Place.create!(country: 'USA', city: 'Los Angeles')
-burrillville= Place.create!(country: 'USA', city: 'Burillville')
+burrillville = Place.create!(country: 'USA', city: 'Burillville')
 bronx = Place.create!(country: 'USA', city: 'Bronx')
 
 loew_jersey = Scene.new(
@@ -37,7 +39,7 @@ loew_jersey = Scene.new(
   rating: 4.4,
   price: "9€"
 )
-loew_jersey.photo.attach(URI.open("https://jerseydigs.com/wp-content/uploads/2021/02/Loews-Jersey-Theatre-Journal-Square-Jersey-City-1.jpg"))
+loew_jersey.photo.attach(io: URI.open("https://jerseydigs.com/wp-content/uploads/2021/02/Loews-Jersey-Theatre-Journal-Square-Jersey-City-1.jpg"), filename: "loew_jersey.png", content_type: "image/png")
 loew_jersey.save!
 
 escaliers = Scene.new(
@@ -48,7 +50,7 @@ escaliers = Scene.new(
   content: "Le célèbre escalier qui illustre l'affiche du film",
   price: "Gratuit"
 )
-escaliers.photo.attach(URI.open("https://fr.web.img4.acsta.net/r_654_368/newsv7/19/10/24/14/31/2884301.jpg"))
+escaliers.photo.attach(io: URI.open("https://fr.web.img4.acsta.net/r_654_368/newsv7/19/10/24/14/31/2884301.jpg"), filename: "escaliers.png", content_type: "image/png")
 escaliers.save!
 
 paramount_theater = Scene.new(
@@ -58,7 +60,7 @@ paramount_theater = Scene.new(
   content: "C'est ici qu'Arthur Fleck a été dupé par des adolescents alors qu'il travaillait en tant que clown devant le Newark Paramount Theater.",
   price: "Gratuit"
 )
-paramount_theater.photo.attach(URI.open("https://advancelocal-adapter-image-uploads.s3.amazonaws.com/image.nj.com/home/njo-media/width2048/img/essex_impact/photo/2018/10/12/24954100-standard.jpg"))
+paramount_theater.photo.attach(io: URI.open("https://advancelocal-adapter-image-uploads.s3.amazonaws.com/image.nj.com/home/njo-media/width2048/img/essex_impact/photo/2018/10/12/24954100-standard.jpg"), filename: "paramount_theater.png", content_type: "image/png")
 paramount_theater.save!
 
 conjuring = Scene.new(
@@ -69,7 +71,7 @@ conjuring = Scene.new(
   rating: 4.9,
   price: "25-30€"
 )
-conjuring.photo.attach(URI.open("https://imageio.forbes.com/specials-images/imageserve/629003d4d07c7f069f74921f/Haunted-House--Colonial-house/960x0.jpg?format=jpg&width=960https://i.dailymail.co.uk/1s/2022/06/28/05/59606005-10959397-image-a-73_1656392097884.jpg"))
+conjuring.photo.attach(io: URI.open("https://imageio.forbes.com/specials-images/imageserve/629003d4d07c7f069f74921f/Haunted-House--Colonial-house/960x0.jpg?format=jpg&width=960https://i.dailymail.co.uk/1s/2022/06/28/05/59606005-10959397-image-a-73_1656392097884.jpg"), filename: "conjuring.png", content_type: "image/png")
 conjuring.save!
 
 venice_beach = Scene.create(
@@ -79,6 +81,6 @@ venice_beach = Scene.create(
   content: "Barbie à Venice Beach, un lieu dynamique qui reflète la diversité culturelle du monde réel.",
   price: "Gratuit"
 )
-venice_beach.photo.attach(URI.open("https://i.dailymail.co.uk/1s/2022/06/28/05/59606005-10959397-image-a-73_1656392097884.jpg"))
+venice_beach.photo.attach(io: URI.open("https://i.dailymail.co.uk/1s/2022/06/28/05/59606005-10959397-image-a-73_1656392097884.jpg"), filename: "venice_beach.png", content_type: "image/png")
 venice_beach.save!
 end

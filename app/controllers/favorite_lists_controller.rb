a1b61e950b0b8c2431147c24a8403740619998e9
lilian chabaus <lilian.chbd@gmail.com>
2023-11-30 14:24:31 +0100

    m

--- app/controllers/favorite_lists_controller.rb
+++ app/controllers/favorite_lists_controller.rb
@@ -1,11 +1,32 @@
 class FavoriteListsController < ApplicationController
+  before_action :authenticate_user!, only: [:create]
+
+  def index
+    @favorite_lists = current_user.favorite_lists
+  end
+
+  def show
+    @favorite_list = current_user.favorite_lists.find(params[:id])
+  end
+
   def create
-    @favorite_list = FavoriteList.new(favorite_list_params)
+    @favorite_list = current_user.favorite_lists.build(favorite_list_params)
+
     if @favorite_list.save
       redirect_to @favorite_list
     else
-      render :new
+      render :new, status: :unprocessable_entity
     end
+  end
+
+  def new
+    @favorite_list = FavoriteList.new
+  end
+
+  def destroy
+    @favorite_list = current_user.favorite_lists.find(params[:id])
+    @favorite_list.destroy
+    redirect_to favorite_lists_path
   end
 
   private

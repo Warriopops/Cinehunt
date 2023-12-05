39491eef865ce7e3236998fe42d9747060dfef3f
Hilham Behillil <hilhambehillilcw@gmail.com>
2023-12-05 12:43:07 +0100

    révision du carousel du home page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
+    @user_favorite_lists = current_user.favorite_lists
+    @last_favorite_lists = FavoriteList.last(3)
   end
 end

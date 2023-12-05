42d5db1aec80b0f9ac62b91fbb0c6aa37f3ad4a9
Hilham Behillil <hilhambehillilcw@gmail.com>
2023-12-05 15:24:48 +0100

    problème réglé

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,9 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
+    ids = Scene.pluck(:id)
+    @first_scene = Scene.find(ids.delete(ids.sample))
+    @second_scene = Scene.find(ids.delete(ids.sample))
+    @third_scene = Scene.find(ids.delete(ids.sample))
   end
 end

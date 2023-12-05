cbea6c82463557c8440d6c9c6dbfdae6fb7292c4
thomas laizé <thomas33130@hotmail.fr>
2023-12-05 17:28:20 +0100

    save

---
+++ app/javascript/controllers/backfutur_controller.js
@@ -0,0 +1,17 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+  static targets = ["voiture1", "voiture2"]
+  connect() {
+    console.log("FUTUUUR")
+  }
+  test(){
+    console.log("yyyeees")
+    this.voiture1Target.classList.add("d-none");
+    this.voiture2Target.classList.remove("d-none");
+    setTimeout(() => {
+      window.location.href = '/scenes'
+    }, 300);
+
+  }
+}

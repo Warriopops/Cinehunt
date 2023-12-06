7555801791f8e939ff1e51544663120a77a101c4
lilian chabaus <lilian.chbd@gmail.com>
2023-12-07 00:01:06 +0100

    m

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,23 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  protect_from_forgery with: :exception
+  before_action :store_user_location!, if: :storable_location?
+
+  private
+
+  def store_user_location!
+    store_location_for(:user, request.fullpath)
+  end
+
+  def after_sign_in_path_for(resource_or_scope)
+    stored_location_for(resource_or_scope) || super
+  end
+
+  def redirect_back_or_default(default, options = {})
+    redirect_to(session.delete(:return_to) || default, options)
+  end
+
+  def storable_location?
+    request.get? && is_navigational_format? && !devise_controller? && !request.xhr?
+  end
 end
